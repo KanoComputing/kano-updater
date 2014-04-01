a3e6ee64e42a118a74b986a3d658494954e9a92f
Radek Pazdera <radek@kano.me>
2014-04-01 18:08:12 +0100

    lib: Adding the Utils module

---
+++ kano_updater/Utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# kano-extras Python library
+#
+# Copyright (C) 2014 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+#
+# Utilities for the updater and the pre and post update scripts
