e7afae6281400eca1567ca3fc6c0a6b48ad82089
Zsolt Ero <zsolt.ero@gmail.com>
2014-04-22 10:16:18 +0100

    dpkg_dict moved, from utils import ...

--- kano_updater/utils.py
+++ kano_updater/utils.py
@@ -8,7 +8,7 @@
 # Utilities for the updater and the pre and post update scripts
 #
 
-from kano.utils import run_print_output_error
+from kano.utils import run_print_output_error, run_cmd
 
 
 def install(pkgs):
@@ -27,3 +27,24 @@ def remove(pkgs):
 
 def purge(pkgs):
     pass  # TODO
+
+
+def get_dpkg_dict():
+    apps_ok = dict()
+    apps_other = dict()
+
+    cmd = 'dpkg -l'
+    o, _, _ = run_cmd(cmd)
+    lines = o.splitlines()
+    for l in lines[5:]:
+        parts = l.split()
+        state = parts[0]
+        name = parts[1]
+        version = parts[2]
+
+        if state == 'ii':
+            apps_ok[name] = version
+        else:
+            apps_other[name] = version
+
+    return apps_ok, apps_other
