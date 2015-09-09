93daac8349427559c7a9b14f942b385ceea12592
Radu <radu@kano.me>
2015-09-09 18:09:33 +0100

    Added shutdown scheduling for updates and automatic downloading

--- kano_updater/ui/views/finish.py
+++ kano_updater/ui/views/finish.py
@@ -12,21 +12,29 @@ import os
 from threading import Timer
 
 from kano_updater.ui.views.countdown import Countdown
+from kano_updater.paths import SCHEDULE_SHUTDOWN_FILE_PATH
 
-class Restart(Countdown):
+
+class Finish(Countdown):
     def __init__(self):
         Countdown.__init__(self)
 
         complete = Gtk.Label(_('Update complete!'))
         complete.get_style_context().add_class('complete')
 
+        self._shutdown_scheduled = os.path.exists(SCHEDULE_SHUTDOWN_FILE_PATH)
+        finish_method = 'restart'
+
+        if self._shutdown_scheduled:
+            finish_method = 'shutdown'
+
         info = Gtk.Label(_('Your Kano is up to date and \n'
-                           'will automatically restart in 10 seconds'))
+                           'will automatically {} in 10 seconds'.format(finish_method)))
         info.set_justify(Gtk.Justification.CENTER)
         info.get_style_context().add_class('countdown')
 
-        instructions = Gtk.Label(_('Press ENTER to restart now'))
-        instructions.get_style_context().add_class('restart-now')
+        instructions = Gtk.Label(_('Press ENTER to {} now'.format(finish_method)))
+        instructions.get_style_context().add_class('finish-now')
 
         self._main_grid.attach(complete, 0, 1, 1, 1)
         self._main_grid.attach(info, 0, 2, 1, 1)
@@ -35,7 +43,10 @@ class Restart(Countdown):
         self.connect('show', self._on_show)
 
     def _on_show(self, widget=None):
-        self.get_toplevel().connect('key-press-event', self._reboot)
+        if self._shutdown_scheduled:
+            self.get_toplevel().connect('key-press-event', self._shutdown)
+        else:
+            self.get_toplevel().connect('key-press-event', self._reboot)
         self._start_timer()
 
     def _start_timer(self):
@@ -44,7 +55,13 @@ class Restart(Countdown):
         timer.start()
 
     def _reboot(self, window=None, event=None):
-        if event and event.get_keycode()[1] != 36: # ENTER
+        if event and event.get_keycode()[1] != 36:  # ENTER
             return
 
         os.system('reboot')
+
+    def _shutdown(self, window=None, event=None):
+        if event and event.get_keycode()[1] != 36:  # ENTER
+            return
+
+        os.system('poweroff')
--- lxpanel-plugin/kano_updater.c
+++ lxpanel-plugin/kano_updater.c
@@ -43,14 +43,17 @@
 #define UPDATE_STATUS_FILE "/var/cache/kano-updater/status.json"
 
 #define CHECK_FOR_UPDATES_CMD "sudo /usr/bin/kano-updater check --gui"
+/* TODO: #define CHECK_FOR_URGENT_UPDATES_CMD "sudo /usr/bin/kano-updater check --gui" */
 #define DOWNLOAD_CMD "sudo /usr/bin/kano-updater download --low-prio"
 #define INSTALL_CMD "sudo /usr/bin/kano-updater install --gui --no-confirm"
 #define SOUND_CMD "/usr/bin/aplay /usr/share/kano-media/sounds/kano_open_app.wav"
 
 #define PLUGIN_TOOLTIP _("Kano Updater")
 
-#define POLL_INTERVAL (10 * 60 * 1000) /* 10 minutes in microseconds*/
+/* Frequency with which we check for updates depending on priorities */
+#define POLL_INTERVAL (10 * 60 * 1000) /* 10 minutes in microseconds */
 #define CHECK_INTERVAL (60 * 60 * 24 * 7) /* 7 days */
+#define URGENT_CHECK_INTERVAL (60 * 60 * 24) /* 1 day */
 
 #define FIFO_FILENAME ".kano-notifications.fifo"
 
@@ -109,6 +112,7 @@ typedef struct {
     gchar *prev_state;
     int last_update;
     int last_check;
+    int last_check_urgent;
     int notifications_muted;
 
     GtkWidget *icon;
@@ -142,6 +146,7 @@ static GtkWidget *plugin_constructor(LXPanel *panel, config_setting_t *settings)
     plugin_data->prev_state = g_new0(gchar, MAX_STATE_LENGTH);
     plugin_data->last_update = 0;
     plugin_data->last_check = 0;
+    plugin_data->last_check_urgent = 0;
     plugin_data->notifications_muted = FALSE;
 
     GtkWidget *icon = gtk_image_new_from_file(NO_UPDATES_ICON_FILE);
@@ -246,9 +251,14 @@ static void launch_cmd(const char *cmd, const char *appname)
 static gboolean check_for_updates(kano_updater_plugin_t *plugin_data)
 {
     int now = time(NULL);
+    /* TODO */
+    // if ((now - plugin_data->last_check_urgent) >= URGENT_CHECK_INTERVAL) {
+    //     launch_cmd(CHECK_FOR_URGENT_UPDATES_CMD, NULL);
+    // }
     if ((now - plugin_data->last_check) >= CHECK_INTERVAL) {
         launch_cmd(CHECK_FOR_UPDATES_CMD, NULL);
     }
+
     return TRUE;
 }
 
@@ -315,6 +325,9 @@ static gboolean read_status(kano_updater_plugin_t *plugin_data)
 
             plugin_data->last_check = (int)
                 json_object_get_number(root, "last_check");
+
+            plugin_data->last_check_urgent = (int)
+                json_object_get_number(root, "last_check_urgent");
 
             plugin_data->last_update = (int)
                 json_object_get_number(root, "last_update");
