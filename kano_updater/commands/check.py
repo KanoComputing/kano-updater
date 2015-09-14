8571cec7d4106df6e7ea029fb4fba701f4f306d0
Tom Bettany <tom@kano.me>
2015-09-14 14:37:39 +0100

    Priority: Mute notifications during urgent update
    KanoComputing/engage#62
    The urgent updates use dialogs rather than notifications so they
    persist. This means that when the state changes the triggered
    notifications aren't valid so mute them when entering this state.

--- kano_updater/commands/check.py
+++ kano_updater/commands/check.py
@@ -64,6 +64,7 @@ def check_for_updates(progress=None, priority=Priority.NONE):
     update_type = _do_check(progress, priority=priority)
     if update_type == Priority.NONE:
         status.state = UpdaterStatus.NO_UPDATES
+        status.notifications_muted = True
         logger.debug('No updates available')
         rv = False
     else:
