c74cdafc523c3047de29f1c49c4c17d95b5378da
Radu <radu@kano.me>
2015-09-14 18:32:32 +0100

    Moved install scheduling from shutdown to boot

--- kano_updater/ui/views/restart.py
+++ kano_updater/ui/views/restart.py
@@ -11,30 +11,22 @@ from gi.repository import Gtk
 import os
 from threading import Timer
 
-from kano_updater.commands.install import is_scheduled
 from kano_updater.ui.views.countdown import Countdown
 
-
-class Finish(Countdown):
+class Restart(Countdown):
     def __init__(self):
         Countdown.__init__(self)
 
         complete = Gtk.Label(_('Update complete!'))
         complete.get_style_context().add_class('complete')
 
-        self._shutdown_scheduled = is_scheduled()
-        finish_method = 'restart'
-
-        if self._shutdown_scheduled:
-            finish_method = 'shutdown'
-
         info = Gtk.Label(_('Your Kano is up to date and \n'
-                           'will automatically {} in 10 seconds'.format(finish_method)))
+                           'will automatically restart in 10 seconds'))
         info.set_justify(Gtk.Justification.CENTER)
         info.get_style_context().add_class('countdown')
 
-        instructions = Gtk.Label(_('Press ENTER to {} now'.format(finish_method)))
-        instructions.get_style_context().add_class('finish-now')
+        instructions = Gtk.Label(_('Press ENTER to restart now'))
+        instructions.get_style_context().add_class('restart-now')
 
         self._main_grid.attach(complete, 0, 1, 1, 1)
         self._main_grid.attach(info, 0, 2, 1, 1)
@@ -43,19 +35,16 @@ class Finish(Countdown):
         self.connect('show', self._on_show)
 
     def _on_show(self, widget=None):
-        self.get_toplevel().connect('key-press-event', self._finish)
+        self.get_toplevel().connect('key-press-event', self._reboot)
         self._start_timer()
 
     def _start_timer(self):
-        timer = Timer(11, self._finish, ())
+        timer = Timer(11, self._reboot, ())
         timer.daemon = True
         timer.start()
 
-    def _finish(self, window=None, event=None):
-        if event and event.get_keycode()[1] != 36:  # ENTER
+    def _reboot(self, window=None, event=None):
+        if event and event.get_keycode()[1] != 36: # ENTER
             return
 
-        if self._shutdown_scheduled:
-            os.system('poweroff')
-        else:
-            os.system('reboot')
+        os.system('reboot')
