7ab864accfac8733258357f4d9ee11e29db5aeb6
Radu <radu@kano.me>
2016-01-12 15:20:14 +0000

    Updated license headers

--- kano_updater/scenarios.py
+++ kano_updater/scenarios.py
@@ -1,7 +1,7 @@
 
 # scenarios.py
 #
-# Copyright (C) 2014, 2015 Kano Computing Ltd.
+# Copyright (C) 2014-2016 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 #
 
