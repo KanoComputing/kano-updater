fcb67f35ca5573083f2a6e88eaa5a6c552e98b50
Alex Burr <alex.burr@kano.me>
2016-03-09 01:45:51 +0000

    Include list of 'special' packages  which don't trigger an OS update.

---
+++ kano_updater/special_packages.py
@@ -0,0 +1,9 @@
+#
+# special_packages.py
+#
+# Copyright (C) 2014-2015 Kano Computing Ltd.
+# License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
+#
+# List of packages which require special treatment
+
+independent_install_list = ['kano-overworld']
