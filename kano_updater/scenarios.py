08bedce8bf4c821cda60e838200f6de3ccf9e6fe
Albert Casals <skarbat@gmail.com>
2018-02-15 18:42:59 +0100

    Moving kano-os-recovery as a dependency instead (#284)

--- kano_updater/scenarios.py
+++ kano_updater/scenarios.py
@@ -375,9 +375,7 @@ class PreUpdate(Scenarios):
         pass
 
     def beta_3_14_1_to_beta_3_15_0(self):
-        # Kano OS recovery system will repair the kit
-        # in case of a power failure during the upgrade
-        install('kano-os-recovery')
+        pass
 
     def _finalise(self):
         # When bluez is installed through a dependency it fails to configure
