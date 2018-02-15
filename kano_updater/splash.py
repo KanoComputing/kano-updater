bef5b023f6a5db45ed5a60d40968e9d4c47955f6
Alex Burr <alex.burr@kano.me>
2018-02-15 14:39:29 +0000

    Allow splash to be selected on the basis of language.

--- kano_updater/splash.py
+++ kano_updater/splash.py
@@ -7,12 +7,15 @@
 # configure boot splash
 
 import os
-from paths import INTERRUPTED_SPLASH_PATH
+from paths import INTERRUPTED_SPLASH_PATH, INTERRUPTED_SPLASH_FILENAME
+from kano_i18n import assets
 
 
 def set_splash_interrupted():
     # This command may not exist yet, so ignore errors
-    os.system('kano-boot-splash-cli set {}'.format(INTERRUPTED_SPLASH_PATH))
+    splash_path = assets.get_path(INTERRUPTED_SPLASH_PATH,
+                                  INTERRUPTED_SPLASH_FILENAME)
+    os.system('kano-boot-splash-cli set {}'.format(splash_path))
 
 
 def clear_splash():
