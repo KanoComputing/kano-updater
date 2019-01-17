ee7ff19449cd2d0b5de6acacce648980f82649d7
Radu Jipa <radu.t.jipa@gmail.com>
2019-01-17 18:39:46 +0000

    Increased the minimum disk requirement buffer to 850MB for dpkg
    This also removes the MIN_REQ_SPACE as it now incorporates it.
    Reason for the change is that dpkg seems to be requiring extra
    space to unpack and install certain packages. This was measured
    in an update from 3.16.1 to 4.2.0 to be somewhat larger than the
    previous amount.

--- kano_updater/disk_requirements.py
+++ kano_updater/disk_requirements.py
@@ -1,10 +1,10 @@
 # disk_requirements.py
 #
-# Copyright (C) 2018 Kano Computing Ltd.
+# Copyright (C) 2018-2019 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 #
 # Variables pertaining to required disk usage for upgrade
 
-SPACE_BUFFER = 100  # MB
-MIN_REQ_SPACE = 500  # MB
+
+SPACE_BUFFER = 850  # MB
 UPGRADE_3_8_0_SPACE = 1000  # MB
