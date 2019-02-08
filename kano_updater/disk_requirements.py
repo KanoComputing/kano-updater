6246078b100d1240ea355191c166e5d74e316a90
Radu Jipa <radu.t.jipa@gmail.com>
2019-02-08 11:23:44 +0000

    Add logging w.r.t. the final value of the reuqired disk space

--- kano_updater/disk_requirements.py
+++ kano_updater/disk_requirements.py
@@ -6,6 +6,9 @@
 # Variables pertaining to required disk usage for upgrade
 
 
+from kano.logging import logger
+
+
 SPACE_BUFFER = 850  # MB
 UPGRADE_3_8_0_SPACE = 1000  # MB
 
@@ -29,6 +32,8 @@ def check_disk_space(priority):
     system_version = get_system_version()
     if system_version < OSVersion.from_version_string("Kanux-Beta-3.8.0"):
         required_space += UPGRADE_3_8_0_SPACE
+
+    logger.info('Final upgrade required size is {} MB'.format(required_space))
 
     if mb_free < required_space:
         err_msg = N_("Only {}MB free, at least {}MB is needed.").format(
