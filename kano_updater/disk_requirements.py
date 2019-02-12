034115a98ac8a7933d8f252cdac2c2cead05ad41
Radu Jipa <radu.t.jipa@gmail.com>
2019-02-12 17:17:20 +0000

    Removed v3.7 to v3.8 3P app installation disk space requirement
    Before, the disk space requirement was part of the update itself.
    Moved away from that model to install apps separately of the update
    to minimise the requirement (divide and conquer).
    However, the App Store does not check for disk space before
    installing an app so account for this here naively.
    Requirements (disk_req) in MB were made with apt on a clean system.

--- kano_updater/disk_requirements.py
+++ kano_updater/disk_requirements.py
@@ -3,14 +3,13 @@
 # Copyright (C) 2018-2019 Kano Computing Ltd.
 # License: http://www.gnu.org/licenses/gpl-2.0.txt GNU GPL v2
 #
-# Variables pertaining to required disk usage for upgrade
+# Required disk usage for upgrade
 
 
 from kano.logging import logger
 
 
 SPACE_BUFFER = 850  # MB
-UPGRADE_3_8_0_SPACE = 1000  # MB
 
 
 def check_disk_space(priority):
@@ -21,17 +20,10 @@ def check_disk_space(priority):
     from kano.utils.disk import get_free_space
 
     from kano_updater.apt_wrapper import AptWrapper
-    from kano_updater.os_version import get_system_version, OSVersion
 
     apt_handle = AptWrapper.get_instance()
     mb_free = get_free_space()
     required_space = apt_handle.get_required_upgrade_space() + SPACE_BUFFER
-
-    # Allowance for installing extra packages in the postupdate scenarios
-    # during the update to 3.8.0
-    system_version = get_system_version()
-    if system_version < OSVersion.from_version_string("Kanux-Beta-3.8.0"):
-        required_space += UPGRADE_3_8_0_SPACE
 
     logger.info('Final upgrade required size is {} MB'.format(required_space))
 
